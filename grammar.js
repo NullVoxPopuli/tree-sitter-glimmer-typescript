a43f2fa2f0c70a8ae7f1bb8111301c432c5209be
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-08-17 15:04:49 -0400

    formatting

--- grammar.js
+++ grammar.js
@@ -1,19 +1,20 @@
 "use strict";
 
-const TypeScript = require('tree-sitter-typescript').typescript;
+const TypeScript = require("tree-sitter-typescript").typescript;
 
+console.log(TypeScript);
 /**
-  * A <template></template> block can exist in
-  * two types of locations:
-  * 1. class body
-  * 2. any expression
-  *
-  * The contents of `<template>contents</template>`
-  * are handled by tree-sitter-glimmer
-  *   https://github.com/ember-tooling/tree-sitter-glimmer
-  */
+ * A <template></template> block can exist in
+ * two types of locations:
+ * 1. class body
+ * 2. any expression
+ *
+ * The contents of `<template>contents</template>`
+ * are handled by tree-sitter-glimmer
+ *   https://github.com/ember-tooling/tree-sitter-glimmer
+ */
 module.exports = grammar(TypeScript, {
-  name: 'glimmer.typescript',
+  name: "glimmer.typescript",
 
   /**
    * TODO: add support for attributes
@@ -25,39 +26,40 @@ module.exports = grammar(TypeScript, {
    *       Either will require RFC
    *         https://github.com/emberjs/rfcs/
    */
-  glimmer_template: $ => choice(
-    seq(
-      field('open_tag', $.glimmer_opening_tag),
-      field('content', repeat($._glimmer_template_content)),
-      field('close_tag', $.glimmer_closing_tag),
+  glimmer_template: ($) =>
+    choice(
+      seq(
+        field("open_tag", $.glimmer_opening_tag),
+        field("content", repeat($._glimmer_template_content)),
+        field("close_tag", $.glimmer_closing_tag),
+      ),
+      // empty template has no content
+      // <template></template>
+      seq(
+        field("open_tag", $.glimmer_opening_tag),
+        field("close_tag", $.glimmer_closing_tag),
+      ),
     ),
-    // empty template has no content
-    // <template></template>
-    seq(
-      field('open_tag', $.glimmer_opening_tag),
-      field('close_tag', $.glimmer_closing_tag),
-    ),
-  ),
 
-  _glimmer_template_content: _ => /.{1,}/,
-  glimmer_opening_tag: _ => '<template>',
-  glimmer_closing_tag: _ => '</template>',
+  _glimmer_template_content: (_) => /.{1,}/,
+  glimmer_opening_tag: (_) => "<template>",
+  glimmer_closing_tag: (_) => "</template>",
 
   /**
-  * 2. Any Expression.
-  * e.g.:
-  *
-  *   ```gts
-  *   export const Foo = <template>...</template>
-  *   ```
-  *
-  * TS: https://github.com/tree-sitter/tree-sitter-typescript/blob/master/common/define-grammar.js#L212
-  * JS: https://github.com/tree-sitter/tree-sitter-javascript/blob/master/grammar.js#L479
-  */
+   * 2. Any Expression.
+   * e.g.:
+   *
+   *   ```gts
+   *   export const Foo = <template>...</template>
+   *   ```
+   *
+   * TS: https://github.com/tree-sitter/tree-sitter-typescript/blob/master/common/define-grammar.js#L212
+   * JS: https://github.com/tree-sitter/tree-sitter-javascript/blob/master/grammar.js#L479
+   */
   expression: ($, previous) => {
     const choices = [
       $.glimmer_template,
-      ...previous.members.filter(member => member.name !== '_jsx_element'),
+      ...previous.members.filter((member) => member.name !== "_jsx_element"),
     ];
 
     return choice(...choices);
@@ -69,18 +71,23 @@ module.exports = grammar(TypeScript, {
    *
    * Most of thish is copied from upstream (TS), and only $.glimmer_template is added
    *
-  * TS: https://github.com/tree-sitter/tree-sitter-typescript/blob/master/common/define-grammar.js#L419
-  * JS: https://github.com/tree-sitter/tree-sitter-javascript/blob/master/grammar.js#L1150
-  */
-  class_body: $ => seq(
-        '{',
-        repeat(choice(
+   * TS: https://github.com/tree-sitter/tree-sitter-typescript/blob/master/common/define-grammar.js#L419
+   * JS: https://github.com/tree-sitter/tree-sitter-javascript/blob/master/grammar.js#L1150
+   */
+  class_body: ($) =>
+    seq(
+      "{",
+      repeat(
+        choice(
           seq(
-            repeat(field('decorator', $.decorator)),
+            repeat(field("decorator", $.decorator)),
             $.method_definition,
             optional($._semicolon),
           ),
-          seq($.method_signature, choice($._function_signature_automatic_semicolon, ',')),
+          seq(
+            $.method_signature,
+            choice($._function_signature_automatic_semicolon, ","),
+          ),
           $.class_static_block,
           seq(
             choice(
@@ -89,13 +96,12 @@ module.exports = grammar(TypeScript, {
               $.method_signature,
               $.public_field_definition,
             ),
-            choice($._semicolon, ','),
+            choice($._semicolon, ","),
           ),
-          field('template', $.glimmer_template),
-          ';',
-        )),
-        '}',
+          field("template", $.glimmer_template),
+          ";",
+        ),
       ),
-
-
-})
+      "}",
+    ),
+});
